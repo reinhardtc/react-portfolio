1ecc11f047923a7e26e12286af3a7c564e8abc13
reinhardtc <reinhardtc22@gmail.com>
2021-08-13 16:06:30 -0500

    added first components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from '../src/components/Nav';
+import About from '../src/components/About';
+import Contact from '../src/components/Contact';
+import Portfolio from '../src/components/Portfolio';
+import Home from '../src/components/Home';
+import Footer from '../src/components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Using useState, set the default value for currentPage to 'Home'
+	const [currentPage, handlePageChange] = useState('Home');
+	// The renderPage method uses a switch statement to render the appropriate current page
+	const renderPage = () => {
+		switch (currentPage) {
+			case 'About':
+				return <About />;
+			case 'Portfolio':
+				return <Portfolio />;
+			case 'Contact':
+				return <Contact />;
+			default:
+				return <Home />;
+		}
+	};
+	return (
+		<div>
+			<main>
+				<Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+
+				<div>{renderPage(currentPage)}</div>
+
+				<Footer></Footer>
+			</main>
+		</div>
+	);
 }
 
 export default App;
