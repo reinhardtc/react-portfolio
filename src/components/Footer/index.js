60a9e4d5bfcbff9f256f8735db0594a4258ba6cf
reinhardtc <reinhardtc22@gmail.com>
2021-10-02 16:35:55 -0500

    styled components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,12 @@ function Footer() {
   return (
     <footer>
       <div className="social-links">
-        <SocialIcon url="https://www.linkedin.com/in/colinreinhardt/" target="_blank" />
-        <SocialIcon url="https://github.com/reinhardtc" target="_blank" />
+        <SocialIcon
+          className="linkedin"
+          url="https://www.linkedin.com/in/colinreinhardt/"
+          target="_blank"
+        />
+        <SocialIcon className="github" url="https://github.com/reinhardtc" target="_blank" />
       </div>
     </footer>
   );
