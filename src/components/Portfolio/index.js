f51bd879b086071654cc7197dbc9ba6ed7fc09a8
colin reinhardt <reinhardtc22@gmail.com>
2022-01-31 12:54:15 -0600

    updated projects page grid layout

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
-import cleanest from '../../assets/cleanest.png';
 // import mealMatch from '../../assets/meal-match.png';
-// import myJober from '../../assets/myJober.png';
 // import noteTaker from '../../assets/noteTaker.png';
 
 function Portfolio() {
-  return (
-    <section>
-      <div className="projectsHeader">
-        <h1 className="projectsHeading">My Projects</h1>
-      </div>
-      <hr></hr>
-      <div className="projectsContainer">
-        <div className="projectBox cleanest">
-          <image src={cleanest} alt="application photo" className="cleanestImage"></image>
-        </div>
-        <div className="projectBox mealMatch"></div>
-        <div className="projectBox myJober"></div>
-        <div className="projectBox noteTaker"></div>
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div className="projectsHeader">
+				<h1 className="projectsHeading">My Projects</h1>
+			</div>
+			<hr></hr>
+			<div className="projectsContainer">
+				<div className="projectBox mealMatch"></div>
+				<div className="projectBox cleanest"></div>
+				<div className="projectBox reactBudget"></div>
+				<div className="projectBox noteTaker"></div>
+			</div>
+		</section>
+	);
 }
 
 export default Portfolio;
