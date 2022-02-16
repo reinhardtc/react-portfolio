110fa2b5114e6a149386bc4334d1330d692f6d37
Colin Reinhardt <reinhardtc22@gmail.com>
2022-02-16 17:32:20 -0600

    added resume download link to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import resume from '../../assets/resume.pdf';
 
 function Footer() {
-	return (
-		<footer>
-			<h3 className="titles">Let's talk!</h3>
-			<div className="contactInfo">
-				<a className="contacts" href="tel:+8604599777">
-					860-459-9777
-				</a>
-				<a className="contacts" href="mailto:reinhardtc22@gmail.com">
-					reinhardtc22@gmail.com
-				</a>
-			</div>
-			<div className="social-links">
-				<SocialIcon
-					className="linkedin"
-					url="https://www.linkedin.com/in/colinreinhardt/"
-					target="_blank"
-				/>
-				<SocialIcon className="github" url="https://github.com/reinhardtc" target="_blank" />
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <h3 className='titles'>Let's talk!</h3>
+      <div className='contactInfo'>
+        <a className='contacts' href='tel:+8604599777'>
+          860-459-9777
+        </a>
+        <a className='contacts' href='mailto:reinhardtc22@gmail.com'>
+          reinhardtc22@gmail.com
+        </a>
+        <a href={resume} target='_blank'>
+          Download my resume here
+        </a>
+      </div>
+      <div className='social-links'>
+        <SocialIcon
+          className='linkedin'
+          url='https://www.linkedin.com/in/colinreinhardt/'
+          target='_blank'
+        />
+        <SocialIcon
+          className='github'
+          url='https://github.com/reinhardtc'
+          target='_blank'
+        />
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
